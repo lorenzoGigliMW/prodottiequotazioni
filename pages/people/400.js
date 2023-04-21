847977457614953fb93a155096a10e5040c55ef0
lorenzoGigliMW <lorenzo.gigli@markertwall.com>
2023-04-21 16:32:54 +0200

    riedit

---
+++ pages/people/400.js
@@ -0,0 +1,15 @@
+import {useRouter} from 'next/router';
+import {useEffect} from 'react';
+
+export default function NotFound() {
+    const router=useRouter();
+    useEffect (()=>{                               //avviato solo una volta all'inizializzazione della pagina
+        setTimeout(()=>{router.push('/')},2000); //dopo 2 secondi mi fa ritornare nella home
+    },[])
+    return (
+        <>
+            <h1>400 - Bad Request</h1>
+            <p> Non ho trovato la pagina</p>
+        </>
+    );
+}
