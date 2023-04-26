2f5960d79a701c11f077b0ddba5dcc2c4c2b1d42
lorenzoGigliMW <lorenzo.gigli@markertwall.com>
2023-04-26 17:37:42 +0200

    vf

--- components/imi/HeaderPEQ.js
+++ components/imi/HeaderPEQ.js
@@ -1,52 +1,52 @@
 import Link from 'next/link';
 import Image from 'next/image'
 
-export default function Nav(){
-    return(
+export default function Nav() {
+    return (
         <div>
-<nav>
-    <ul>
-        <li><Link href="/home"><a>IMI CORPORATE & INVESTMENT BANKING</a></Link></li>
-        <li><Link href="/chisiamo"><a>CHI SIAMO</a></Link></li>
-        <li><Link href="/news"><a>NEWS</a></Link></li>
-        <li><Link href="/contatti"><a>CONTATTI</a></Link></li>
-        <li><Image     
-      src="nomeimmagine.png"
-      alt="cornetta"   //fill
-      width={100}      // sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw,33vw" 
-      height={100}
-    /></li>
-        <li><Link href="/numero"><a>800.303.303</a></Link></li>
-    </ul>
-</nav><hr />
-<div>
+            <nav>
+                <ul>
+                    <li><Link href="/home"><a>IMI CORPORATE & INVESTMENT BANKING</a></Link></li>
+                    <li><Link href="/chisiamo"><a>CHI SIAMO</a></Link></li>
+                    <li><Link href="/news"><a>NEWS</a></Link></li>
+                    <li><Link href="/contatti"><a>CONTATTI</a></Link></li>
+                    <li><Image
+                        src="nomeimmagine.png"
+                        alt="cornetta"   //fill
+                        width={100}      // sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw,33vw" 
+                        height={100}
+                    /></li>
+                    <li><Link href="/numero"><a>800.303.303</a></Link></li>
+                </ul>
+            </nav><hr />
+            <div>
 
-<Image     
-      src="imi.png"
-      alt="cornetta"   //fill
-      width={300}      // sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw,33vw" 
-      height={300}
-    />
+                <Image
+                    src="imi.png"
+                    alt="cornetta"   //fill
+                    width={300}      // sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw,33vw" 
+                    height={300}
+                />
 
-<Image     
-      src="intesasanpaolo.png"
-      alt="intesaSP"   //fill
-      width={300}      // sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw,33vw" 
-      height={300}
-    />
-</div>
-<hr />
-<div>
-<Image     
-      src="grafico.png"
-      alt="grafico"   //fill
-      width={100}      // sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw,33vw" 
-      height={100}
-    />
-    <h2>Prodotti e Quotazioni</h2>
-</div>
+                <Image
+                    src="intesasanpaolo.png"
+                    alt="intesaSP"   //fill
+                    width={300}      // sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw,33vw" 
+                    height={300}
+                />
+            </div>
+            <hr />
+            <div>
+                <Image
+                    src="grafico.png"
+                    alt="grafico"   //fill
+                    width={100}      // sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw,33vw" 
+                    height={100}
+                />
+                <h2>Prodotti e Quotazioni</h2>
+            </div>
 
-{/* <div className="NavigazionePagine">
+            {/* <div className="NavigazionePagine">
 <nav>
     <ul>
         <li><Link href="/home"><a>Prodotti</a></Link></li>
@@ -57,6 +57,6 @@ export default function Nav(){
     </ul>
 </nav>
 </div> */}
-</div>
-);
+        </div>
+    );
 }
--- components/imi/NavigazionePag.js
+++ components/imi/NavigazionePag.js
@@ -2,18 +2,18 @@ import Link from 'next/link';
 
 export default function NavigazionePag() {
     return (
-        <div>
-            <div className="NavigazionePagine">
-                <nav>
-                    <ul>
-                        <li><Link href="/home"><a>Prodotti</a></Link></li>
-                        <li><Link href="/chisiamo"><a>News e Formazione</a></Link></li>
-                        <li><Link href="/news"><a>Studi e Ricerche</a></Link></li>
-                        <li><Link href="/contatti"><a>Documentazione</a></Link></li>
-                        <li><Link href="../pages/imi/cercaNelSito"><a>Cerca nel sito</a></Link></li>
-                    </ul>
-                </nav>
-            </div>
+
+        <div className="NavigazionePagine">
+            <nav>
+                <ul>
+                    <li><Link href="/home"><a>Prodotti</a></Link></li>
+                    <li><Link href="/chisiamo"><a>News e Formazione</a></Link></li>
+                    <li><Link href="/news"><a>Studi e Ricerche</a></Link></li>
+                    <li><Link href="/contatti"><a>Documentazione</a></Link></li>
+                    <li><Link href="../pages/imi/cercaNelSito"><a>Cerca nel sito</a></Link></li>
+                </ul>
+            </nav>
         </div>
+
     );
 }
